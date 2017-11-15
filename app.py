96d919b82548ac1596ff9afd0350ce716587308f
Diego <diego.parejo.carrasco@gmx.de>
2017-11-15 14:53:12 +0100

    added raster2array convert

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class Point:
 def get_array_from_dataset(dataset):
     band = dataset.GetRasterBand(1)
     array = band.ReadAsArray()
+    #_display_array(array)
     return array
 
 
@@ -25,15 +26,21 @@ def get_dataset_from_filepath(filepath):
     )
     return dataset
 
+def convert_Tiff_raster_to_array(user_input_path):
+    raster = gdal.Open(user_input_path)
+    band = raster.GetRasterBand(1)
+    array = band.ReadAsArray()
+    return array
+
 
 def get_path_from_cost_surface(cost_dataset, start_point, dest_point):
-    CostSurfacefn = "C:\\Users\\Diego\\Desktop\\python_workspace\\bsp.tif"
+    CostSurfacefn = "C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif"
     # coordinates to array index
     startCoordX = start_point.x
     startCoordY = start_point.y
     startIndexX, startIndexY = cookbook.coord2pixelOffset(
-        CostSurfacefn, 
-        startCoordX, 
+        CostSurfacefn,
+        startCoordX,
         startCoordY
     )
 
@@ -45,12 +52,13 @@ def get_path_from_cost_surface(cost_dataset, start_point, dest_point):
         stopCoordY
     )
 
-    costSurfaceArray = cookbook.raster2array(CostSurfacefn)
-
+    costSurfaceArray = convert_Tiff_raster_to_array(CostSurfacefn)
+    _display_array(costSurfaceArray)
+    print("##############")
     # create path
     indices, weight = route_through_array(
-        costSurfaceArray, 
-        (startIndexY, startIndexX), 
+        costSurfaceArray,
+        (startIndexY, startIndexX),
         (stopIndexY, stopIndexX),
         geometric=True,
         fully_connected=True
@@ -58,29 +66,38 @@ def get_path_from_cost_surface(cost_dataset, start_point, dest_point):
     indices = numpy.array(indices).T
     path = numpy.zeros_like(costSurfaceArray)
     path[indices[0], indices[1]] = 1
+    _display_array(path)
     return path
 
 
-def conver_array_to_Tiff_raster(array,output_path):
+
+
+def convert_array_to_Tiff_raster(array, output_path):
     driver = gdal.GetDriverByName('GTiff')
     cols = array.shape[1]
     rows = array.shape[0]
     outRaster = driver.Create(output_path, cols, rows, 1, gdal.GDT_Byte)
     return outRaster
 
+
+def _display_array(array):
+    for row in array:
+        for pixel in row:
+            print(pixel, end="")
+        print()
+
 def main():
 
-    dataset = get_dataset_from_filepath("C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif")
-    path_dataset = get_path_from_cost_surface(dataset, Point(0, 0), Point(10, 10))
-    print(path_dataset)
-    print(path_dataset[10, 10])
+    user_input_path = "C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif"
+
+    data_raster = get_dataset_from_filepath("C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif")
+
+    data_array = convert_Tiff_raster_to_array(user_input_path)
+
+    path_dataset = get_path_from_cost_surface(data_array, Point(0, 0), Point(9, 9))
 
     output_path = "C:\\Users\\Diego\\Desktop\\python_workspace\\path_raster.tif"
-    path_raster = conver_array_to_Tiff_raster(path_dataset, output_path)
-     
-    # data_array = get_array_from_dataset(path_dataset) 
-    
-    # print(data_array[0, 0])
+    path_raster = convert_array_to_Tiff_raster(path_dataset, output_path)
 
 if __name__ == '__main__':
     main()
--- app_test.py
+++ app_test.py
@@ -4,7 +4,7 @@ from app import *
 import cookbook
 
 class AppTests(unittest.TestCase):
-    
+
     def test_import_dataset_given_filepath_should_return_something(self):
         filepath = "C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif"
         dataset = get_dataset_from_filepath(filepath)
@@ -18,6 +18,15 @@ class AppTests(unittest.TestCase):
         expected_pixel_value = 68
         actual_pixel_value = get_array_from_dataset(dataset)[0, 0]
         self.assertEqual(expected_pixel_value, actual_pixel_value)
+
+    def test_import_dataset_given_10by10_should_return_10by10_dataset(self):
+        filepath = "C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif"
+        dataset = get_dataset_from_filepath(filepath)
+
+        array = get_array_from_dataset(dataset)
+        self.assertEqual(len(array), 10)
+        self.assertEqual(len(array[0]), 10)
+
 
     def test_get_path_given_raster_and_points_should_return_non_empty_dataset(self):
         filepath = "C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif"
@@ -59,19 +68,19 @@ class AppTests(unittest.TestCase):
         actual_pixel_value = expected[9, 9]
         self.assertEqual(expected_pixel_value, actual_pixel_value)
 
-        expected_pixel_value = 0
-        actual_pixel_value = expected[10, 10]
-        self.assertEqual(expected_pixel_value, actual_pixel_value)
-
     def test_create_raster_given_array_and_path(self):
         expected = None
         dataset = get_dataset_from_filepath("C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif")
-        dataset_array = get_path_from_cost_surface(dataset, Point(0, 0), Point(10, 10))
+        dataset_array = get_path_from_cost_surface(dataset, Point(0, 0), Point(9, 9))
         output_path = "C:\\Users\\Diego\\Desktop\\python_workspace\\raster_bsp.tif"
-        raster = conver_array_to_Tiff_raster(dataset_array, output_path)        
+        raster = convert_array_to_Tiff_raster(dataset_array, output_path)
         self.assertIsNot(raster, expected)
-        
-        
+
+    def test_create_array_given_path_should_return_array(self):
+        expected = None
+        data_array = convert_Tiff_raster_to_array("C:\\Users\\Diego\\Desktop\\python_workspace\\10x10_bsp.tif")
+        self.assertIsNot(data_array, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
-
